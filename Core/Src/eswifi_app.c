34960951f02bff36b4367364ae0fc3421953c201
Isidre Solé Ramon <isole@sistemeskairos.cat>
2021-07-26 16:06:55 +0200

    Simplificacio de la lectura per IRQ en esWifiParser

--- Core/Src/eswifi_app.c
+++ Core/Src/eswifi_app.c
@@ -222,11 +222,9 @@ int8_t esWifiParse(int8_t *message)
 	uint16_t size;
 
 	memset(eswifi_app_buf.rxData,'\0', ESWIFI_APP_BUF_SIZE);
+	ret = HAL_UART_Receive_IT(&huart1,aux, 1);   //  Aqui tindras que posar el buffer i una longitud de 1 caracter.
 	while(!esWifiRxFlag)
-	{
-		if (esWifiRx(&rxBuffDummy, 0xffff) == ESWIFI_TIMEOUT)
-			break;
-	}
+        HAL_Delay(10);
 
 	if (esWifiRxFlag)
 	{
